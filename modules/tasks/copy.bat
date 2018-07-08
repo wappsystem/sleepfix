097663a438535afa15dc08703f0f221a3ed53594
Zhiming Zhou <zzm.vmiis@gmail.com>
2018-07-08 14:38:00 +1000

    update

--- modules/tasks/copy.bat
+++
@@ -1,9 +0,0 @@
-set S=%USERPROFILE%\Documents\GitHub\wappsystem\sleepfix-management\modules\tasks
-set D=%USERPROFILE%\Documents\GitHub\wappsystem\sleepfix\modules\tasks\mirror
-
-xcopy %S%\screening-form.html %D%\screening-form.html /D
-xcopy %S%\pis-form.html %D%\pis-form.html /D
-xcopy %S%\consent-form.html %D%\consent-form.html /D
-xcopy %S%\registration-form.html %D%\registration-form.html /D
-
-pause
