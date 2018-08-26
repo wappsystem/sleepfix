9faef6dabc5319f1570cbb0bb119a24e4916de0c
Gunnar Unger <gunnar.unger@gmail.com>
2018-08-27 09:26:41 +1000

    update

--- modules/tasks/copy to mirror.bat
+++ modules/tasks/copy to mirror.bat
@@ -6,5 +6,8 @@ xcopy %S%\screening-form.html %D%\screening-form.html /D /Y
 xcopy %S%\pis-form.html %D%\pis-form.html /D /Y
 xcopy %S%\consent-form.html %D%\consent-form.html /D /Y
 xcopy %S%\registration-form.html %D%\registration-form.html /D /Y
+xcopy %S%\screen-failure-phq-9.html %D%\screen-failure-phq-9.html /D /Y
+xcopy %S%\screen-failure-thank-you-page.html %D%\screen-failure-thank-you-page.html /D /Y
+xcopy %S%\sleepfix-study-page.html %D%\sleepfix-study-page.html /D /Y
 
 pause
